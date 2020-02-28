2db4ddabfb0506895f38431a152d196b93592839
Dellamoresteven <dellamoresteven@gmail.com>
2020-02-28 01:12:36 -0500

    More

--- input/text.cpp
+++ input/text.cpp
@@ -1,11 +1,21 @@
+// #frontpage \ 
 // @author Steven Dellamore \ 
-// @date Feb 27th 2020 \ 
+// @date Feb 28th 2020 \ 
 // @version 1.0.0 \ 
-// @company Garmin \ 
-// @title Intro to Doc \ 
+// @company CS 352 \ 
+// @title ahh \ 
 // @location West Lafayette, IN \ 
 // @email dellamoresteven@gmail.com \ 
-// @frontpage
+
+// #class \ 
+// @name MyClass \ 
+// @date Feb 25th 2020 \ 
+// @version 1.1.0 \ 
+// @desc This is my desc \ 
+// @author Steven Dellamore \ 
+class object {
+
+};
 
 int main(){ // hellp
 
